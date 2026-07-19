0a3acc86af6753a01759c5ac9dd61b6c0a59c29f
Eisi05 <clechi05@gmail.com>
2026-07-19 16:57:54 +0200

    Fixed NPC loading

--- src/main/java/de/eisi05/npc/api/manager/NpcManager.java
+++ src/main/java/de/eisi05/npc/api/manager/NpcManager.java
@@ -77,6 +77,8 @@ public class NpcManager
     public static void clear()
     {
         npcById.clear();
+        toLoadNPCs.clear();
+        loadExceptions.clear();
     }
 
     /**
@@ -239,7 +241,9 @@ public class NpcManager
             if(either.left().isEmpty())
                 return false;
 
-            loadNpc(either.left().get());
+            NPC npc = either.left().get();
+            npc.markChange();
+            loadNpc(npc);
             return true;
         }
         catch(Exception e)
