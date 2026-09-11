acb29475c87c78ec4f700c8f774bb83b8091c754
Eisi05 <clechi05@gmail.com>
2026-09-11 20:52:04 +0200

    Added Exception handling

--- src/main/java/de/eisi05/npc/api/utils/serialize/ItemSerializer.java
+++ src/main/java/de/eisi05/npc/api/utils/serialize/ItemSerializer.java
@@ -9,10 +9,9 @@ import org.jetbrains.annotations.Nullable;
 import java.util.Base64;
 
 /**
- * The {@link ItemSerializer} class provides utility methods for serializing and deserializing
- * Bukkit {@link ItemStack} arrays, {@link PlayerInventory} contents, and generic {@link Inventory}
- * contents to and from Base64 encoded strings. This is useful for storing inventory data
- * in configurations or databases.
+ * The {@link ItemSerializer} class provides utility methods for serializing and deserializing Bukkit {@link ItemStack} arrays, {@link PlayerInventory}
+ * contents, and generic {@link Inventory} contents to and from Base64 encoded strings. This is useful for storing inventory data in configurations or
+ * databases.
  */
 public class ItemSerializer
 {
@@ -20,8 +19,7 @@ public class ItemSerializer
      * Serializes a single {@link ItemStack} into a Base64 encoded string.
      *
      * @param item The {@link ItemStack} to serialize. Must not be {@code null}.
-     * @return A Base64 encoded {@link String} representing the item.
-     * Returns {@code null} if an error occurs during serialization.
+     * @return A Base64 encoded {@link String} representing the item. Returns {@code null} if an error occurs during serialization.
      */
     public static @NotNull String itemStackToBase64(@NotNull ItemStack item)
     {
@@ -29,15 +27,21 @@ public class ItemSerializer
     }
 
     /**
-     * Deserializes a single {@link ItemStack} from a Base64 encoded string.
-     * This method expects the Base64 string to represent a single item.
+     * Deserializes a single {@link ItemStack} from a Base64 encoded string. This method expects the Base64 string to represent a single item.
      *
      * @param data The Base64 encoded {@link String} representing the item. Must not be {@code null}.
-     * @return The deserialized {@link ItemStack}, or {@code null} if an error occurs during deserialization
-     * or if the input data does not represent a valid item.
+     * @return The deserialized {@link ItemStack}, or {@code null} if an error occurs during deserialization or if the input data does not represent a valid
+     * item.
      */
     public static @Nullable ItemStack itemStackFromBase64(@NotNull String data)
     {
-        return ItemStack.deserializeBytes(Base64.getMimeDecoder().decode(data));
+        try
+        {
+            return ItemStack.deserializeBytes(Base64.getMimeDecoder().decode(data));
+        }
+        catch(Exception e)
+        {
+            return null;
+        }
     }
 }
